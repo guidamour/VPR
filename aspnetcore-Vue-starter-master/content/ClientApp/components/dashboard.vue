80ffe7ee882809c5b702bbfdae563688e58fc5d5
damourg <gdamour@ssmic.com>
2018-06-29 14:13:56 -0400

    Adding a component to test vuetify

---
+++ aspnetcore-Vue-starter-master/content/ClientApp/components/dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-container grid-list-md text-xs-center>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <v-card dark color="blue">
+                    <h1>this is a test</h1>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
